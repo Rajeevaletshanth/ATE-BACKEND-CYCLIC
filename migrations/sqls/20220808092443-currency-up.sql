728fb0e549e3619c73299f8944e2073ddbc4280c
LTW-TECH <admin@ltwtech.it>
2022-11-13 14:43:12 +0100

    added

--- migrations/sqls/20220808092443-currency-up.sql
+++ migrations/sqls/20220808092443-currency-up.sql
@@ -1,15 +1,21 @@
 /* Replace with your SQL commands */
-CREATE TABLE IF NOT EXISTS public.cuisines
+-- Table: public.admin
+
+-- DROP TABLE IF EXISTS public.admins;
+
+CREATE TABLE IF NOT EXISTS public.currency
 (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    image VARCHAR(200),
+    code VARCHAR(10) NOT NULL,
+    status INTEGER NOT NULL,
     is_deleted BOOLEAN DEFAULT false,
     created_at TIMESTAMP,
     updated_at TIMESTAMP 
 );
 
+
 -- TABLESPACE pg_default;
 
--- ALTER TABLE IF EXISTS public.cuisines
+-- ALTER TABLE IF EXISTS public.currency
 --     OWNER to postgres;
