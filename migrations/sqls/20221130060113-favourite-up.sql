71f228ec1b516fc2bfcf50e1d87e44d358c20daf
ajith <ajith.y@hotmail.com>
2022-11-30 11:55:00 +0530

    db

--- migrations/sqls/20221130060113-favourite-up.sql
+++ migrations/sqls/20221130060113-favourite-up.sql
@@ -1,1 +1,32 @@
 /* Replace with your SQL commands */
+
+CREATE TABLE IF NOT EXISTS public.order
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    price FLOAT NOT NULL,
+    order_date date,
+    order_time time,
+    delivery_fee FLOAT,
+    total_amount FLOAT NOT NULL,
+    status INTEGER NOT NULL,
+    is_deleted BOOLEAN DEFAULT false,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP 
+);
+
+CREATE TABLE IF NOT EXISTS public.favourite
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    restaurant_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    status INTEGER NOT NULL,
+    is_deleted BOOLEAN DEFAULT false,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP 
+);
+
