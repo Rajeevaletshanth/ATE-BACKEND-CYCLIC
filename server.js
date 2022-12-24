a2fccc6c9c647a7ce7bdda94a22228080448e75f
Ramachandran_Rajeevaletshanth <78897410+Rajeevaletshanth@users.noreply.github.com>
2022-12-24 14:58:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -246,7 +246,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-server.listen(process.env.PORT, () => console.log(`Real Estate Tokenization engine on live on port ${process.env.PORT}!`))
+server.listen(process.env.PORT || 5005, () => console.log(`Real Estate Tokenization engine on live on port ${process.env.PORT || 5005}!`))
 
 module.exports = app;
 
